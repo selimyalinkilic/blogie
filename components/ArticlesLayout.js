42a1d37088407f576d786a6e77158bc7358f50a5
selimyalinkilic <yalinkilicselim@gmail.com>
2022-06-02 00:19:29 +0300

    Change filter id

--- components/ArticlesLayout.js
+++ components/ArticlesLayout.js
@@ -12,7 +12,7 @@ const contentStyle = (contents, classList, newClassList) => {
 };
 
 const ArticlesLayout = ({ title, hasFilter, content }) => {
-  const [active, setActive] = React.useState(["newest"]);
+  const [active, setActive] = React.useState(["published_at_DESC"]);
   const [data, setData] = React.useState([]);
   const [loading, setLoading] = React.useState(true);
   const [message, setMessage] = React.useState("");
@@ -37,28 +37,28 @@ const ArticlesLayout = ({ title, hasFilter, content }) => {
 
   return (
     <div className="flex flex-col items-center lg:items-start lg:ml-72 xl:ml-80 px-4 md:px-5 lg:p-8 w-full">
-      <h2 className="text-3xl lg:text-4xl">{title || "-"}</h2>
+      <h2 className="text-3xl lg:text-4xl capitalize">{title || "-"}</h2>
       {hasFilter && data && data.length > 0 && (
         <div className="flex items-center mt-4 mt:md-5 lg:mt-6">
           <button
             className={`text-base text-black pb-1 border-b-2 ${
-              active.includes("newest")
+              active.includes("published_at_DESC")
                 ? "opacity-100 border-black"
                 : "opacity-60 border-transparent"
             }`}
             onClick={handleClick}
-            id="newest"
+            id="published_at_DESC"
           >
             Newest
           </button>
           <span className="px-2 text-black opacity-50 text-base pb-1">-</span>
           <button
             className={`text-base text-black pb-1 border-b-2 ${
-              active.includes("oldest")
+              active.includes("published_at_ASC")
                 ? "opacity-100 border-black"
                 : "opacity-60 border-transparent"
             }`}
-            id="oldest"
+            id="published_at_ASC"
             onClick={handleClick}
           >
             Oldest
